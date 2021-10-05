3e9c378087889c79d4f60d1e5c4f92273995ef28
Rahul <rahulkvr7a.rnj@gmail.com>
2021-10-05 12:26:38 +0530

    5th

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.render('admin/admin-login', { admin: true })
+  res.render('admin/admin-login',)
 });
 
-router.post('/login', (req, res) => {
+router.post('/dashboard', (req, res) => {
   res.render('admin/admin-dashboard', { admin: true })
 })
 
-
+router.get('/dashboard', (req, res) => {
+  res.render('admin/admin-dashboard', { admin: true })
+})
 
 router.get('/orders', (req, res) => {
   res.render('admin/admin-orders', { admin: true })
@@ -20,9 +22,27 @@ router.get('/products', (req, res) => {
   res.render('admin/admin-products', { admin: true })
 })
 
-router.get('/addproduct', () => {
-  // res.render('admin/admin-addproduct', { admin: true })
-  res.render("Edit user")
+router.get('/addproduct', (req, res) => {
+  res.render('admin/admin-addproduct', { admin: true })
 })
+
+router.get('/users', (req, res) => {
+  res.render('admin/admin-user', { admin: true })
+})
+
+router.get('/editproduct', (req, res) => {
+  res.render('admin/admin-editproduct', { admin: true })
+})
+
+router.get('/orderdetails', (req, res) => {
+  res.render('admin/admin-orderdetails', { admin: true })
+})
+
+router.get('/offers', (req, res) => {
+  res.send('coming soon')
+})
+
+
+
 
 module.exports = router;
