b1e8a1fbc614095ae44274c5f34b2672de58b493
Rahul <rahuljayaraman8@gmail.com>
2021-11-10 13:43:09 +0530

    mongoose

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state = {
     db: null
 }
 module.exports.connect = (done) => {
-    const connection_url = 'mongodb://localhost:27017'
+    const connection_url = 'mongodb+srv://rahul:12345@cluster0.6xw0g.mongodb.net/test'
     const dbname = 'ecommerce'
 
     mongoClient.connect(connection_url, (err, data) => {
